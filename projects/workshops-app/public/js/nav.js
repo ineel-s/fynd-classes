a5104ed57189cb1a7be9f1ee9f1d3afaff3a2c51
Prashanth Puranik <puranik@digdeeper.in>
2022-10-19 19:04:25 +0530

    responsive menu

---
+++ projects/workshops-app/public/js/nav.js
@@ -0,0 +1,11 @@
+// querySelector returns a single object (DOM node)
+const toggler = document.querySelector( '.main-menu-toggler' );
+
+toggler.addEventListener( 'click', function() {
+    // querySelectorAll returns an array of objects (DOM nodes)
+    const mainMenuLinks = document.querySelectorAll( '.main-menu-link' );
+
+    for( let i = 0; i < mainMenuLinks.length; i++ ) {
+        mainMenuLinks[i].classList.toggle( 'd-sm-none' );
+    }
+} );
