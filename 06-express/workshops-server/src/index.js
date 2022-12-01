c99ae75a39f2f24e4291f7883af3ccfa8e9b9fae
Prashanth Puranik <puranik@digdeeper.in>
2022-12-01 19:54:43 +0530

    basic routing

--- 06-express/workshops-server/src/index.js
+++ 06-express/workshops-server/src/index.js
@@ -3,9 +3,15 @@ const express = require( 'express' );
 // Create an application object
 const app = express();
 
+// app.<http_method>()
+app.get( '/', ( req, res ) => {
+    res.write( 'Hello Express' );
+    res.end( 'More to learn...' )
+});
+
 const PORT = process.env.PORT || 3000;
 
 // creates a web server and starts that internally
 app.listen( PORT, () => {
-    console.log( `server started on http://localhost:${PORT}` );
+    console.log( `server started on - http://localhost:${PORT}` );
 });
